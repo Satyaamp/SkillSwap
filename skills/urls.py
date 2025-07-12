ac91bfa16330052996476efec462192d6beabe63
Sayantan De <105956142+sayout-de003@users.noreply.github.com>
2025-07-12 12:31:38 +0530

    Delete skills directory

--- skills/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add/', views.add_skill, name='add_skill'),
-    path('browse/', views.browse_skills, name='browse_skills'),
-]
